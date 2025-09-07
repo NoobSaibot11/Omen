0c8d4f56a90c555b21a89c426958778a0b693aca
Majahar <majahar1152003@gmail.com>
2025-09-07 21:51:40 +0530

    - Added landing screen animation - Added usernmae and password input fields

--- source/screens/LandingScreen/LandingScreen.tsx
+++ source/screens/LandingScreen/LandingScreen.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { BaseScreen } from '../../components';
 import LinearGradient from 'react-native-linear-gradient';
 import { LandingScreenProps } from './types';
 
+import styles from './styles';
+import OmenAnimation from './components/OmenAnimation';
+
 const LandingScreen = ({ navigation }: LandingScreenProps) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onPressSignIn = () => {
+    navigation.navigate('AppStack', { screen: 'Home' });
+  };
+
   return (
     <BaseScreen theme="dark" backgroundColor="#4B117F">
       <LinearGradient
         colors={['#4B117F', '#290a4e', '#040112']}
-        style={{ flex: 1 }}
+        style={styles.AnimationGradientWrapper}
       >
-        <Text style={{ color: 'white' }}>Landing</Text>
-        <TouchableOpacity
-          onPress={() => {
-            console.log('Hello');
-            navigation.navigate('AppStack', { screen: 'Home' });
-          }}
+        <ScrollView
+          contentContainerStyle={styles.ScrollViewStyle}
+          keyboardShouldPersistTaps="handled"
         >
-          <Text>Done</Text>
-        </TouchableOpacity>
+          <OmenAnimation />
+
+          <View style={styles.TextWrapper}>
+            <Text style={styles.Texts}>Welcome</Text>
+            <Text style={styles.Texts}>to Omen</Text>
+          </View>
+
+          <View style={styles.InputWrapper}>
+            <LinearGradient
+              colors={['#260848', '#1A083C']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.GradientStyle}
+            >
+              <TextInput
+                placeholder="USERNAME"
+                placeholderTextColor="#71409B"
+                style={styles.TextInputStyle}
+                value={username}
+                onChangeText={setUsername}
+              />
+            </LinearGradient>
+          </View>
+
+          <View style={[styles.InputWrapper, styles.InputWrapper2]}>
+            <LinearGradient
+              colors={['#260848', '#1A083C']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.GradientStyle}
+            >
+              <TextInput
+                placeholder="PASSWORD"
+                placeholderTextColor="#71409B"
+                style={styles.TextInputStyle}
+                value={password}
+                secureTextEntry
+                onChangeText={setPassword}
+              />
+            </LinearGradient>
+          </View>
+
+          <View style={styles.LineStyle} />
+
+          <TouchableOpacity
+            onPress={onPressSignIn}
+            style={styles.ButtonWrapper}
+          >
+            <View style={styles.ButtonBox}>
+              <Text style={styles.ButtonText}>SIGN IN</Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
       </LinearGradient>
     </BaseScreen>
   );
---
+++ source/screens/LandingScreen/styles.ts
@@ -0,0 +1,70 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  AnimationGradientWrapper: {
+    flex: 1,
+  },
+  ScrollViewStyle: {
+    flexGrow: 1,
+  },
+  AnimationWrapper: {
+    width: '100%',
+    marginBottom: 40,
+    alignItems: 'center',
+  },
+  ImageProps: {
+    width: 180,
+    height: 200,
+    overflow: 'hidden',
+  },
+  TextWrapper: {
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  Texts: {
+    color: 'white',
+    fontSize: 40,
+  },
+  InputWrapper: {
+    marginHorizontal: 40,
+    borderWidth: 1,
+    borderColor: '#2A1376',
+  },
+  InputWrapper2: {
+    marginTop: 25,
+  },
+  GradientStyle: {
+    height: 65,
+    justifyContent: 'center',
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+  TextInputStyle: {
+    fontSize: 24,
+    color: 'white',
+  },
+  LineStyle: {
+    borderTopWidth: 4,
+    borderColor: '#231538',
+    marginTop: 50,
+    marginHorizontal: 40,
+  },
+  ButtonWrapper: {
+    alignSelf: 'center',
+    marginTop: 40,
+  },
+  ButtonBox: {
+    width: 155,
+    height: 60,
+    backgroundColor: '#191A1C',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ButtonText: {
+    color: 'white',
+    fontSize: 24,
+  },
+});
+
+export default styles;
